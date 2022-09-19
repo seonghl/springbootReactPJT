6b9c4bedf1a4cb78d4d929b29b8543268d95699b
leem <qkdrlqnd@naver.com>
2022-09-19 21:06:26 +0900

    최초 설정

---
+++ src/main/java/com/react/pjt/ReactPjtApplication.java
@@ -0,0 +1,13 @@
+package com.react.pjt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactPjtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactPjtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/react/pjt/ReactPjtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.react.pjt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactPjtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
